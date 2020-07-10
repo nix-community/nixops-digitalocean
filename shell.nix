4347d81d4c5113315f54d47cdf18cd88b3c041e1
adisbladis <adisbladis@gmail.com>
2020-07-10 17:36:03 +0200

    Pin python 3.7
    There are upstream packaging bugs around python 3.8

--- shell.nix
+++ shell.nix
@@ -5,6 +5,7 @@ in pkgs.mkShell {
   buildInputs = [
     (pkgs.poetry2nix.mkPoetryEnv {
       projectDir = ./.;
+      python = pkgs.python37;
       overrides = pkgs.poetry2nix.overrides.withDefaults overrides;
     })
     pkgs.poetry
